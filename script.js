8ef39a93af2e93e1dd0a962a1030bd5fd6690f50
maddnty <nzkb24@gmail.com>
2022-01-23 20:10:37 -0600

    added logic for questions to be built and to fetch from array

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const startBtn = document.querySelector("#startBtn"); 
 const timer = document.querySelector("#timer");
+const content = document.querySelector("#content");
+const qzContent = document.querySelector("#qzContent");
+
+
 const timeRemain = 90;
 
+// start buttons for choices
+for(var j=0 ; j < 4; i++){
+    var subject = document.createElement("div");
+    var opt = document.createElement("button");
+    opt.setAttribute("data-index",i);
+    opt.setAttribute("class", "btn btn-primary rounded-pill mb-2");
+    choiceArray.push(opt);
+    subjectArray.push(subject);
+}
+
+
 function startQz(){
+
     startTmr();
+    buildQuestion();
 
 }
 
@@ -15,12 +32,37 @@ function startTmr(){
 
         timer.textContent = "Time : "+timeRemain+ " seconds";
         if(timeRemain <= 0){
-            viewResult();
             clearInterval(tmrInterval);
             timer.text = "";
         }
     }, 1000);
-    console.log(timer);
+
 }
 
-startBtn.addEventListener("click",startBtn);
+function buildQuestion(){
+
+
+
+    if(questionIndex > totalQuestions =1){
+        return;
+    }
+    else {
+        correct = questions[questionIndex].answer;
+
+        // display question
+        questionEl.innerHTML = questions[questionIndex].title;
+        questionEl.setAttribute("class","text-left");
+        questionEl.style.display= "block";
+
+        for (var i = 0; i <4; i++){
+            var index = choiceArray[i].getAttribute("data-index");
+            choiceArray[i].textContent = (+index+1) + ". "+questions[questionIndex].choices[index];
+            divArray[i].appendChild(choiceArray[i]);
+            qzContent.appendChild(divArray[i]);
+        }
+
+    }
+    content.style.display= "block";
+}
+
+startBtn.addEventListener("click",startQz);
