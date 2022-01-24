4d25f7169c473b50e26f622eb253696ea06c69d7
maddnty <nzkb24@gmail.com>
2022-01-23 18:45:05 -0600

    added timer function, linked start button

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const startBtn = document.querySelector("#startBtn"); 
-const counter = document.querySelector("#timer");
+const timer = document.querySelector("#timer");
+const timeRemain = 90;
 
+function startQz(){
+    startTmr();
+
+}
+
+function startTmr(){
+
+    var tmrInterval = setInterval(function(){
+
+        secondsLeft--;
+
+        timer.textContent = "Time : "+timeRemain+ " seconds";
+        if(timeRemain <= 0){
+            viewResult();
+            clearInterval(tmrInterval);
+            timer.text = "";
+        }
+    }, 1000);
+    console.log(timer);
+}
+
+startBtn.addEventListener("click",startBtn);
